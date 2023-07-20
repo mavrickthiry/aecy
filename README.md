cc1f33bb4c0927c17798f336b195dd2e377155e1
Aecy <devsnake@hotmail.com>
2023-07-20 20:15:04 +0200

    update(template): readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hi 👋, I'm Aecy
+A passionate fullstack developer from Belgium living in France.
+
+
--- templates/readme.js
+++ templates/readme.js
@@ -1,1 +1,2 @@
-module.exports = `# Hi 👋, I'm Aecy`;
+module.exports = `# Hi 👋, I'm Aecy
+A passionate fullstack developer from Belgium :flag_be: living in France.`;
