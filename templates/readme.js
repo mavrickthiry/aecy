2d66339babddd338070adbcddd19f9d9d211ca5b
Aecy <devsnake@hotmail.com>
2023-07-20 20:25:04 +0200

    update(templates): readme

--- templates/readme.js
+++ templates/readme.js
@@ -1,2 +1,6 @@
 module.exports = `# Hi 👋, I'm Aecy
-A passionate fullstack developer from Belgium :flag_be: living in France.`;
+A passionate fullstack developer from Belgium living in France.
+I represent myself as a <#myself>, I've learn Web and I am also a conceptor of virtual product for games servers.
+
+## Contact
+[![Stack Overflow](https://img.shields.io/badge/-Stackoverflow-FE7A16?logo=stack-overflow&logoColor=white)](https://stackoverflow.com/users/15290194) [![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?logo=Twitter&logoColor=white)](https://twitter.com/aecyMV) [![Discord](https://img.shields.io/badge/Discord-%237289DA.svg?logo=discord&logoColor=white)](https://discordapp.com/channels/@me/258295794996609024)`;
