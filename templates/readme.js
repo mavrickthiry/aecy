becd3c008d64ce406dd7a38e17e4b24ad9f4ceb1
Aecy <devsnake@hotmail.com>
2023-07-21 14:19:52 +0200

    update(template): little change

--- templates/readme.js
+++ templates/readme.js
@@ -3,12 +3,12 @@ A passionate fullstack developer from **Belgium** 🍻 living in **France** 🥖
 
 I represent myself as a <#my_self> and also a conceptor of virtual product for online games servers.
 
-## Contact
+## 📑 Contact
 [![Stack Overflow](https://img.shields.io/badge/-Stackoverflow-FE7A16?logo=stack-overflow&logoColor=white)](https://stackoverflow.com/users/15290194) [![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?logo=Twitter&logoColor=white)](https://twitter.com/aecyMV) [![Discord](https://img.shields.io/badge/Discord-%237289DA.svg?logo=discord&logoColor=white)](https://discordapp.com/channels/@me/258295794996609024)
 
 Daytime counter before next year <#day_before_new_years>
 
-## ❤️ Support me
+## ❤️ Support
 - [Buy me a coffee](https://www.buymeacoffee.com/aecy)
 - [Ko-fi](https://ko-fi.com/aecym)
 
